196bd960df32baf01309daaee5d90e02755b3ccc
cervantes21 <admon.owlsoul@gmail.com>
2023-03-20 22:53:38 -0600

    Dataset Cats of world

---
+++ my-movie-api/Cats_of_world.py
@@ -0,0 +1,248 @@
+cats = {
+    
+    "Siamese": {
+        "id": 1,
+        "breed": "siamese",
+        "weight": "8-12 lbs",
+        "size": "Medium",
+        "color": "Seal point, chocolate point, blue point, lilac point"
+    },
+    "Persian": {
+        "id": 2,
+        "breed": "persian",
+        "weight": "7-12 lbs",
+        "size": "Medium to large",
+        "color": "Solid color, shaded and smoke, tabby, bicolor, and Himalayan"
+    },
+    "Bengal": {
+        "id": 3,
+        "breed": "bengal",
+        "weight": "8-15 lbs",
+        "size": "Medium to large",
+        "color": "Brown spotted, snow spotted, silver spotted, and marbled"
+    },
+    "Sphynx": {
+        "id": 4,
+        "breed": "sphynx",
+        "weight": "6-12 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Maine Coon": {
+        "id": 5,
+        "breed": "maine coon",
+        "weight": "13-18 lbs",
+        "size": "Large",
+        "color": "Wide range of colors, including solid, tabby, and tortoiseshell"
+    },
+    "Scottish Fold": {
+        "id": 6,
+        "breed": "scottish fold",
+        "weight": "6-13 lbs",
+        "size": "Medium",
+        "color": "Solid color, bicolor, and tabby"
+    },
+    "Russian Blue": {
+        "id": 7,
+        "breed": "russian blue",
+        "weight": "7-15 lbs",
+        "size": "Medium",
+        "color": "Blue-gray with silver-tipped fur"
+    },
+    "British Shorthair": {
+        "id": 8,
+        "breed": "british shorthair",
+        "weight": "9-18 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors, including solid, bicolor, and tabby"
+    },
+    "Siberian": {
+        "id": 9,
+        "breed": "siberian",
+        "weight": "8-17 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Ragdoll": {
+        "id": 10,
+        "breed": "ragdoll",
+        "weight": "10-20 lbs",
+        "size": "Large",
+        "color": "Pointed colors, including seal, blue, chocolate, and lilac"
+    },
+    "American Shorthair": {
+        "id": 11, 
+        "breed": "american shorthair",
+        "weight": "6-15 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Devon Rex": {
+        "id": 12,
+        "breed": "devon rex",
+        "weight": "5-10 lbs",
+        "size": "Small to medium",
+        "color": "Solid color, tabby, and bicolor"
+    },
+    "Cornish Rex": {
+        "id": 13,
+        "breed": "cornish rex",
+        "weight": "5-10 lbs",
+        "size": "Small to medium",
+        "color": "Solid color, tabby, and bicolor"
+    },
+    "Norwegian Forest Cat": {
+        "id": 14,
+        "breed": "norwegian forest cat",
+        "weight": "9-18 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Turkish Angora": {
+        "id": 16,
+        "breed": "turkish angora",
+        "weight": "5-10 lbs",
+        "size": "Small to medium",
+        "color": "White or white with another color"
+    },
+    "Birman": {
+        "id": 17,
+        "breed": "birman",
+        "weight": "7-20 lbs",
+        "size": "Medium to large",
+        "color": "White with colored markings"
+    },
+    "Himalayan": {
+        "id": 18,
+        "breed": "himalayan",
+        "weight": "6-12 lbs",
+        "size": "Medium",
+        "color": "Pointed colors, including seal, blue, chocolate, and lilac"
+    },
+    "Egyptian Mau": {
+        "id": 19,
+        "breed": "egyptian mau",
+        "weight": "6-14 lbs",
+        "size": "Medium",
+        "color": "Silver, bronze, or smoke with black spots"
+    },
+    "Tonkinese": {
+        "id": 20,
+        "breed": "tonkinese",
+        "weight": "6-12 lbs",
+        "size": "Medium",
+        "color": "Pointed colors, mink, and solid"
+    },
+    "Oriental": {
+        "id": 21,
+        "breed": "oriental",
+        "weight": "5-10 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Balinese": {
+        "id": 22,
+        "breed": "balinese",
+        "weight": "5-10 lbs",
+        "size": "Medium",
+        "color": "Pointed colors, including seal, blue, chocolate, and lilac"
+    },
+    "Manx": {
+        "id": 23,
+        "breed": "manx",
+        "weight": "5-12 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Abyssinian": {
+        "id": 24,
+        "breed": "abyssinian",
+        "weight": "6-10 lbs",
+        "size": "Medium",
+        "color": "Ruddy, red, blue, and fawn"
+    },
+    "Exotic Shorthair": {
+        "id": 25,
+        "breed": "exotic shorthair",
+        "weight": "7-14 lbs",
+        "size": "Medium",
+        "color": "Wide range of colors and patterns, including solid, bicolor, and tabby"
+    },
+    "Chartreux": {
+        "id": 26,
+        "breed": "chartreux",
+        "weight": "7-16 lbs",
+        "size": "Medium",
+        "color": "Blue-gray"
+    },
+    "Pixie-Bob": {
+        "id": 27,
+        "breed": "pixie-bob",
+        "weight": "8-17 lbs",
+        "size": "Medium to large",
+        "color": "Brown spotted tabby with lynx tipping, but can also be black, gray, or white"
+    },
+    "Japanese Bobtail": {
+        "id": 28,
+        "breed": "japanese bobtail",
+        "weight": "6-10 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "LaPerm": {
+        "id": 29,
+        "breed": "laperm",
+        "weight": "6-10 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Munchkin": {
+        "id": 30,
+        "breed": "munchkin",
+        "weight": "4-9 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Peterbald": {
+        "id": 31,
+        "breed": "peterbald",
+        "weight": "6-10 lbs",
+        "size": "Small to medium",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Singapura": {
+        "id": 32,
+        "breed": "singapura",
+        "weight": "4-8 lbs",
+        "size": "Small",
+        "color": "Sepia",
+    },
+    "Burmese": {
+        "id": 33,
+        "breed": "burmese",
+        "weight": "6-12 lbs",
+        "size": "Small to medium",
+        "color": "Sable, blue, champagne, platinum"
+    },
+    "Bombay": {
+        "id": 34,
+        "breed": "bombay",
+        "weight": "6-11 lbs",
+        "size": "Small to medium",
+        "color": "Black"
+    },
+    "American Bobtail": {
+        "id": 35,
+        "breed": "american bobtail",
+        "weight": "7-16 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+    "Selkirk Rex": {
+        "id": 36,
+        "breed": "selkirk rex",
+        "weight": "6-16 lbs",
+        "size": "Medium to large",
+        "color": "Wide range of colors and patterns, including solid, tabby, and tortoiseshell"
+    },
+}
